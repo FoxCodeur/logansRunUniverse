541900700e700d2dc7354ebf78a91319c79ed59a
Daisy <detcherry@gmail.com>
2024-09-19 12:04:43 +0200

    supp compressor

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import compressionPlugin from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(), // Plugin React
-    compressionPlugin({
-      algorithm: "gzip", // Algorithme de compression à utiliser
-      ext: [".js", ".jsx", ".css"], // Extensions de fichiers à compresser
-    }),
+    // Suppression de compressionPlugin ici
   ],
   build: {
     minify: "terser", // Utilisation de Terser pour la minification

c4f7e51fe705f1020b210d1b626385d17a3e85c0
Daisy <detcherry@gmail.com>
2025-06-04 16:07:17 +0200

    mod json config

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/logansRunUniverse/", // Chemin de base pour GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {

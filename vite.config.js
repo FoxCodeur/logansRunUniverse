ea45a118d9532c9a405fc70db49dcbb609872174
Daisy <detcherry@gmail.com>
2025-06-05 16:04:58 +0200

    Préparation du déploiement GitHub Pages : build réussi, ajout ou correction de la configuration, installation et test de gh-pages

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/logansRunUniverse", // Chemin de base pour le déploiement
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // Alias pour éviter les chemins relatifs

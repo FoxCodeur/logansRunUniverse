021362337dc55743a26e9be51d5d2c09393c62e7
Daisy <detcherry@gmail.com>
2025-06-25 18:07:22 +0200

    modif illustration png to webp composant ThinkerBox

--- src/pages/timeline/thinkerBox/ThinkerBox.jsx
+++ src/pages/timeline/thinkerBox/ThinkerBox.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./ThinkerBox.scss";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import thinker0 from "@/assets/images/animationThinkerTimeline/00.png";
-import thinker1 from "@/assets/images/animationThinkerTimeline/01.png";
-import thinker2 from "@/assets/images/animationThinkerTimeline/02.png";
-import thinker3 from "@/assets/images/animationThinkerTimeline/03.png";
-import thinker4 from "@/assets/images/animationThinkerTimeline/04.png";
-import thinker5 from "@/assets/images/animationThinkerTimeline/05.png";
-import thinker6 from "@/assets/images/animationThinkerTimeline/06.png";
-import thinker7 from "@/assets/images/animationThinkerTimeline/07.png";
-import thinker8 from "@/assets/images/animationThinkerTimeline/08.png";
+import thinker0 from "@/assets/images/animationThinkerTimeline/00.webp";
+import thinker1 from "@/assets/images/animationThinkerTimeline/01.webp";
+import thinker2 from "@/assets/images/animationThinkerTimeline/02.webp";
+import thinker3 from "@/assets/images/animationThinkerTimeline/03.webp";
+import thinker4 from "@/assets/images/animationThinkerTimeline/04.webp";
+import thinker5 from "@/assets/images/animationThinkerTimeline/05.webp";
+import thinker6 from "@/assets/images/animationThinkerTimeline/06.webp";
+import thinker7 from "@/assets/images/animationThinkerTimeline/07.webp";
+import thinker8 from "@/assets/images/animationThinkerTimeline/08.webp";
 const ThinkerBox = () => {
   return (
     <>
